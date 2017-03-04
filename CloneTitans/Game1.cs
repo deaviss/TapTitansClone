34451e525d842d925ee5868e871eaca7d0921cf0
Deaviss <deaviss@gmail.com>
2017-03-04 11:41:52 +0100

    d

--- CloneTitans/Game1.cs
+++ CloneTitans/Game1.cs
@@ -412,7 +412,7 @@ namespace CloneTitans
                             else
                                 sb.DrawString(fontS, string.Format("Gives {0} damage\nGives {1} damage over time", FormatNumber(x.DAMAGE), FormatNumber(x.DAMAGE_OVER_TIME)), new Vector2(cursor.X + 12, cursor.Y - 8), Color.Black);
                         }
-                    }
+                    } 
                     break; 
             }
 
